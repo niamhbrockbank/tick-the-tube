be3f378c245901931ae09e158851a4d15e40926f
Niamh Brockbank <niamh.brockbank@imgarena.com>
2025-11-26 15:47:47 +0000

    chore: move countStationStatus to utils

---
+++ src/components/Line/utils/countStationStatus.ts
@@ -0,0 +1,20 @@
+import type { Line, Station } from "../../../store/HomeStore"
+import type { UserStore } from "../../../store/UserVisitationStore"
+
+//TODO: Make the keys related to the StationStatus type instead of hardcoded
+export function countStationStatus(lineId : Line["id"], stations: Station[], stationStatuses: UserStore): {untouched: number, through: number, visited: number}{
+    const statusCount = {
+        untouched : 0,
+        through: 0,
+        visited: 0
+    }
+    
+    for (const station of stations){
+        if (station.lines.includes(lineId)){
+        const status = stationStatuses[station.id]
+        statusCount[status] += 1
+    }
+    }    
+
+    return statusCount
+}
