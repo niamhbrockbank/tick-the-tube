d1ca506138d66daf65f415305bcb49ac2cfbbb43
Niamh Brockbank <niamh.brockbank@imgarena.com>
2025-11-27 13:27:20 +0000

    style: improve statsCard border

--- src/components/Statistics/Statistics.css.ts
+++ src/components/Statistics/Statistics.css.ts
@@ -2,9 +2,6 @@ import { style } from "@vanilla-extract/css";
 
 export const statsSection = style({
      width: '80vw',
-    border: "1px solid black",
-    borderRadius: '8px',
-    padding: "24px 32px",
     margin: "auto"
 })
 
@@ -15,7 +12,9 @@ export const statsBox = style({
 })
 
 export const statsCard = style({
-    border: "1px solid black",
-    borderRadius: '8px',
-    height: "250px"
+    border: "1px solid #e2e8f0",
+    borderRadius: '0.5rem',
+    height: "250px",
+    boxShadow: "0 1px 2px 0 rgb(0 0 0 / 0.05)",
+    color: "#020617"
 })
